53d3a80a61c4b78ccfda2d8bc7797ff0352c0da2
sr494 <suresh.r@dxc.com>
2022-01-31 14:30:04 +0530

    updated

--- src/centos/install/libnss_wrapper.sh
+++ src/centos/install/libnss_wrapper.sh
@@ -10,3 +10,4 @@ echo "add 'souce generate_container_user' to .bashrc"
 
 # have to be added to hold all env vars correctly
 echo 'source $STARTUPDIR/generate_container_user' >> $HOME/.bashrc
+echo 'chromium-browser http://map.norsecorp.com/' >> /etc/init.d/mystartup.sh
